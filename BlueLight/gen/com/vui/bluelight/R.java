54d2c8548d1599ca5cf8c7ed67e50ad0827719f7
hanyonggithub <1039789221@qq.com>
2016-03-17 23:26:23 +0800

    first page

--- BlueLight/gen/com/vui/bluelight/R.java
+++ BlueLight/gen/com/vui/bluelight/R.java
@@ -11,7 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int theme_blue=0x7f040000;
+        public static final int gray_bg=0x7f050001;
+        public static final int theme_blue=0x7f050000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -21,35 +22,58 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
-        public static final int brightness=0x7f020000;
-        public static final int home=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int sw=0x7f020003;
-        public static final int sw_bg=0x7f020004;
+        public static final int back_arr=0x7f020000;
+        public static final int blue_circle=0x7f020001;
+        public static final int brightness=0x7f020002;
+        public static final int home=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int light_off=0x7f020005;
+        public static final int light_on=0x7f020006;
+        public static final int music_off=0x7f020007;
+        public static final int music_on=0x7f020008;
+        public static final int setting=0x7f020009;
+        public static final int sw=0x7f02000a;
+        public static final int sw_bg=0x7f02000b;
     }
     public static final class id {
-        public static final int action_settings=0x7f090005;
-        public static final int rlt_bottom_bar=0x7f090001;
-        public static final int rlt_content=0x7f090000;
-        public static final int tvw_brightness=0x7f090004;
-        public static final int tvw_home=0x7f090002;
-        public static final int tvw_switch=0x7f090003;
+        public static final int action_settings=0x7f0a0011;
+        public static final int back=0x7f0a000e;
+        public static final int group=0x7f0a0006;
+        public static final int lefttime_light=0x7f0a000a;
+        public static final int lefttime_music=0x7f0a000d;
+        public static final int light=0x7f0a0009;
+        public static final int music=0x7f0a000c;
+        public static final int right_btn=0x7f0a0010;
+        public static final int rlt_bottom_bar=0x7f0a0002;
+        public static final int rlt_content=0x7f0a0000;
+        public static final int setting=0x7f0a0007;
+        public static final int title=0x7f0a000f;
+        public static final int topbar=0x7f0a0001;
+        public static final int tvw_brightness=0x7f0a0005;
+        public static final int tvw_home=0x7f0a0003;
+        public static final int tvw_switch=0x7f0a0004;
+        public static final int user_job=0x7f0a000b;
+        public static final int user_name=0x7f0a0008;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int v_topbar=0x7f030001;
+        public static final int home_frg=0x7f030001;
+        public static final int v_topbar=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f090000;
+    }
+    public static final class raw {
+        public static final int aaaass=0x7f040000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060002;
-        public static final int app_name=0x7f060000;
-        public static final int hello_world=0x7f060001;
+        public static final int action_settings=0x7f070002;
+        public static final int app_name=0x7f070000;
+        public static final int hello_world=0x7f070001;
     }
     public static final class style {
         /** 
@@ -73,10 +97,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
 }
--- BlueLight/src/com/vui/bluelight/base/view/TopBarView.java
+++ BlueLight/src/com/vui/bluelight/base/view/TopBarView.java
@@ -1,66 +1,78 @@
 package com.vui.bluelight.base.view;
 
+import com.vui.bluelight.R;
+
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class TopBarView extends RelativeLayout{
-	
-	View leftBtn;
-	View rightBtn;
-	View title;
-	
-	
+public class TopBarView extends RelativeLayout {
+
+	ImageView leftBtn;
+	TextView title;
+	TextView rightBtn;
+
 	public TopBarView(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
+		init(context);
 	}
+
 	public TopBarView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		// TODO Auto-generated constructor stub
+		init(context);
 	}
 
 	public TopBarView(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
 		// TODO Auto-generated constructor stub
+		init(context);
 	}
-	
-	public void init(){
-		if(leftBtn!=null){
-			LayoutParams leftParams=new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-			leftParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
-			this.addView(leftBtn, leftParams);
-		}
-		
 
-		
-		
+	public void init(Context context) {
+		View view = LayoutInflater.from(context).inflate(R.layout.v_topbar, null);
+		this.addView(view);
+		requestLayout();
 	}
 	
-	protected View getLeftBtn() {
+	public void setTitleText(String title){
+		if(title!=null){
+			TextView tvw_title=(TextView) this.findViewById(R.id.title);
+			tvw_title.setText(title);
+		}
+	}
+	
+	public void setRightText(String righttext){
+		if(righttext!=null){
+			TextView tvw_right=(TextView) this.findViewById(R.id.right_btn);
+			tvw_right.setText(righttext);
+		}
+	}
+	
+	public ImageView getLeftBtn(){
+		if(leftBtn==null){
+			leftBtn=(ImageView) findViewById(R.id.back);
+		}
 		return leftBtn;
 	}
-	protected void setLeftBtn(View leftBtn) {
-		this.leftBtn = leftBtn;
-	}
-	protected View getRightBtn() {
-		return rightBtn;
-	}
-	protected void setRightBtn(View rightBtn) {
-		this.rightBtn = rightBtn;
-	}
-	protected View getTitle() {
+	
+	public TextView getTitleView(){
+		if(title==null){
+			title=(TextView) findViewById(R.id.title);
+		}
 		return title;
 	}
-	protected void setTitle(View title) {
-		this.title = title;
+	
+	public TextView getRightBtn(){
+		if(rightBtn==null){
+			rightBtn=(TextView) findViewById(R.id.right_btn);
+		}
+		return rightBtn;
 	}
-	
-
-	
-
-
-
 
 }
