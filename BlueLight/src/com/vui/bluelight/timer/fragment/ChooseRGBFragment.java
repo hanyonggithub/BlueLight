e8f587ac7f083d9c2eff0747a7100c5b32c42176
LiuPan <1573802540@qq.com>
2016-03-19 12:11:54 +0800

    调整头部布局

--- BlueLight/src/com/vui/bluelight/timer/fragment/ChooseRGBFragment.java
+++ BlueLight/src/com/vui/bluelight/timer/fragment/ChooseRGBFragment.java
@@ -18,7 +18,6 @@ public class ChooseRGBFragment extends Fragment{
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		inflater.inflate(R.layout.frag_choose_rgb, null);
 		return inflater.inflate(R.layout.frag_choose_rgb, null);
 	}
 
